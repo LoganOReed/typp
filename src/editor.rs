bc8ced009032966b66d3b19490909a4331219c6c
orlogan <orlogan7@gmail.com>
2022-11-21 06:58:39 -0500

    Added some more cursor movement commands

--- src/editor.rs
+++ src/editor.rs
@@ -42,9 +42,15 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
-            Key::Char('k') | Key::Char('j') | Key::Char('h') | Key::Char('l') => {
-                self.move_cursor(pressed_key)
-            }
+            Key::Char('k')
+            | Key::Char('j')
+            | Key::Char('h')
+            | Key::Char('l')
+            | Key::Char('H')
+            | Key::Char('L')
+            | Key::Char('M')
+            | Key::Char('0')
+            | Key::Char('$') => self.move_cursor(pressed_key),
             _ => (),
         }
         Ok(())
@@ -52,11 +58,28 @@ impl Editor {
 
     fn move_cursor(&mut self, key: Key) {
         let Position { mut y, mut x } = self.cursor_position;
+        let size = self.terminal.size();
+        let height = size.height.saturating_sub(1) as usize;
+        let width = size.width.saturating_sub(1) as usize;
         match key {
             Key::Char('k') => y = y.saturating_sub(1),
-            Key::Char('j') => y = y.saturating_add(1),
+            Key::Char('j') => {
+                if y < height {
+                    y = y.saturating_add(1);
+                }
+            }
             Key::Char('h') => x = x.saturating_sub(1),
-            Key::Char('l') => x = x.saturating_add(1),
+            Key::Char('l') => {
+                if x < width {
+                    x = x.saturating_add(1);
+                }
+            }
+            Key::Char('H') => y = 0,
+            Key::Char('L') => y = height,
+            Key::Char('M') => y = height / 2,
+            Key::Char('0') => x = 0,
+            Key::Char('$') => x = width,
+
             _ => (),
         }
         self.cursor_position = Position { x, y }
