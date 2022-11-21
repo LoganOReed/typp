36acf42e0f3e1c6c5f940a5e9e2794c8d4ae4bad
orlogan <orlogan7@gmail.com>
2022-11-21 10:15:57 -0500

    Fixed Horizontal cursor bug

--- src/editor.rs
+++ src/editor.rs
@@ -100,9 +100,8 @@ impl Editor {
 
     fn move_cursor(&mut self, key: Key) {
         let Position { mut y, mut x } = self.cursor_position;
-        let size = self.terminal.size();
         let height = self.document.len();
-        let width = if let Some(row) = self.document.row(y) {
+        let mut width = if let Some(row) = self.document.row(y) {
             row.len()
         } else {
             0
@@ -127,6 +126,14 @@ impl Editor {
             Key::Char('$') => x = width,
 
             _ => (),
+        }
+        width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
+        if x > width {
+            x = width;
         }
         self.cursor_position = Position { x, y }
     }
