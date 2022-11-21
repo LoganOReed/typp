13131fcf446e3902e9ff2cae6563cf65858d77d4
orlogan <orlogan7@gmail.com>
2022-11-21 06:33:58 -0500

    Movable Cursor

--- src/editor.rs
+++ src/editor.rs
@@ -3,9 +3,15 @@ use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
+pub struct Position {
+    pub x: usize,
+    pub y: usize,
+}
+
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
+    cursor_position: Position,
 }
 
 impl Editor {
@@ -14,6 +20,7 @@ impl Editor {
         Self {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to init terminal."),
+            cursor_position: Position { x: 0, y: 0 },
         }
     }
 
@@ -35,21 +42,36 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Char('k') | Key::Char('j') | Key::Char('h') | Key::Char('l') => {
+                self.move_cursor(pressed_key)
+            }
             _ => (),
         }
         Ok(())
     }
 
+    fn move_cursor(&mut self, key: Key) {
+        let Position { mut y, mut x } = self.cursor_position;
+        match key {
+            Key::Char('k') => y = y.saturating_sub(1),
+            Key::Char('j') => y = y.saturating_add(1),
+            Key::Char('h') => x = x.saturating_sub(1),
+            Key::Char('l') => x = x.saturating_add(1),
+            _ => (),
+        }
+        self.cursor_position = Position { x, y }
+    }
+
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         // handles crashing mid screen refresh
         Terminal::cursor_hide();
-        Terminal::cursor_position(0, 0);
+        Terminal::cursor_position(&Position { x: 0, y: 0 });
         if self.should_quit {
             Terminal::clear_screen();
             println!("Goodbye.\r");
         } else {
             self.draw_rows();
-            Terminal::cursor_position(0, 0);
+            Terminal::cursor_position(&self.cursor_position);
         }
         Terminal::cursor_show();
         Terminal::flush()
